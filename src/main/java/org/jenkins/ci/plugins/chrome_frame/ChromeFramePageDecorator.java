d742400e0ed8cd153a28eb68bcc0411fa6fd0353
Jesse Farinacci <jieryn@gmail.com>
2011-09-22 15:49:36 -0400

    fixing plugin, there was a bad package name

--- src/main/java/org/jenkins/ci/plugins/chrome_frame/ChromeFramePageDecorator.java
+++ src/main/java/org/jenkins/ci/plugins/chrome_frame/ChromeFramePageDecorator.java
@@ -22,147 +22,156 @@
  * THE SOFTWARE.
  */
 
-package org.jenkins.ci.plugins;
+package org.jenkins.ci.plugins.chrome_frame;
 
-import java.util.logging.*;
+import hudson.Extension;
+import hudson.Util;
+import hudson.model.PageDecorator;
 import net.sf.json.JSONObject;
 
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
-import hudson.Extension;
-import hudson.Util;
-import hudson.model.PageDecorator;
-
-import org.jenkins.ci.plugins.chrome_frame.Messages;
-
 /**
- * The
- * <a href="http://wiki.jenkins-ci.org/display/JENKINS/Chrome+Frame+Plugin">Chrome Frame Plugin</a>
- * provides <a href="http://code.google.com/chrome/chromeframe/">Google Chrome Frame</a>
+ * The <a href="http://wiki.jenkins-ci.org/display/JENKINS/Chrome+Frame+Plugin">
+ * Chrome Frame Plugin</a> provides <a
+ * href="http://code.google.com/chrome/chromeframe/">Google Chrome Frame</a>
  * request for all pages.
  */
 @Extension
-public final class ChromeFramePageDecorator extends PageDecorator {
+public final class ChromeFramePageDecorator extends PageDecorator
+{
   /**
    * The default value for {@link #sendHeader}.
    */
-  protected static final boolean DEFAULT_SEND_HEADER=true;
+  protected static final boolean DEFAULT_SEND_HEADER = true;
 
   /**
    * The default value for {@link #sendMeta}.
    */
-  protected static final boolean DEFAULT_SEND_META=true;
+  protected static final boolean DEFAULT_SEND_META   = true;
 
   /**
    * The default value for {@link #sendScript}.
    */
-  protected static final boolean DEFAULT_SEND_SCRIPT=true;
+  protected static final boolean DEFAULT_SEND_SCRIPT = true;
 
   /**
    * The default value for {@link #compatible}.
    */
-  protected static final String DEFAULT_COMPATIBLE = "chrome=1";
+  protected static final String  DEFAULT_COMPATIBLE  = "chrome=1";
 
   /**
    * Whether or not to send the HTTP header.
    */
-  private boolean sendHeader;
+  private boolean                sendHeader;
 
   /**
    * Whether or not to send the HTML meta tag.
    */
-  private boolean sendMeta;
+  private boolean                sendMeta;
 
   /**
    * Whether or not to send the Chrome Frame "Install Now!" javascript.
    */
-  private boolean sendScript;
+  private boolean                sendScript;
 
   /**
-   * What to send to the client, both in the header and the meta, for Chrome Frame compatibility.
+   * What to send to the client, both in the header and the meta, for Chrome
+   * Frame compatibility.
    */
-  private String compatible;
+  private String                 compatible;
 
   /**
    * Create a default Chrome Frame {@link PageDecorator}.
    */
-  public ChromeFramePageDecorator() {
-    this(DEFAULT_SEND_HEADER,DEFAULT_SEND_META,DEFAULT_SEND_SCRIPT, DEFAULT_COMPATIBLE);
+  public ChromeFramePageDecorator()
+  {
+    this(DEFAULT_SEND_HEADER, DEFAULT_SEND_META, DEFAULT_SEND_SCRIPT,
+        DEFAULT_COMPATIBLE);
   }
 
-private static final Logger LOG = Logger.getLogger(ChromeFramePageDecorator.class.getName());
-
   /**
-   * Create a Chrome Frame {@link PageDecorator} with the specified configuration.
+   * Create a Chrome Frame {@link PageDecorator} with the specified
+   * configuration.
    */
   @DataBoundConstructor
-  public ChromeFramePageDecorator(final boolean sendHeader, final boolean sendMeta, final boolean sendScript, final String compatible) {
+  public ChromeFramePageDecorator(final boolean sendHeader,
+      final boolean sendMeta, final boolean sendScript, final String compatible)
+  {
     super();
-LOG.info("ChromeFramePageDecorator(sendHeader=" + sendHeader +", sendMeta=" + sendMeta + ", sendScript=" + sendScript + ", compatible=" + compatible +")");
     load();
     this.sendHeader = sendHeader;
-    this.sendMeta= sendMeta;
+    this.sendMeta = sendMeta;
     this.sendScript = sendScript;
     this.compatible = Util.fixEmpty(compatible);
   }
 
   @Override
-  public String getDisplayName() {
-LOG.info("getDisplayName() ? " + Messages.Chrome_Frame_Plugin_DisplayName());
+  public String getDisplayName()
+  {
     return Messages.Chrome_Frame_Plugin_DisplayName();
   }
 
   @Override
-  public boolean configure(final StaplerRequest request, final JSONObject json) throws FormException {
-LOG.info("configure(request, json=" + json +")");
+  public boolean configure(final StaplerRequest request, final JSONObject json)
+      throws FormException
+  {
     request.bindJSON(this, json);
     save();
     return true;
   }
 
-  public boolean isSendHeader() {
-LOG.info("isSendHeader() ? " + sendHeader);
+  public boolean isSendHeader()
+  {
     return sendHeader;
   }
 
-  public void setSendHeader(final boolean sendHeader) {
+  public void setSendHeader(final boolean sendHeader)
+  {
     this.sendHeader = sendHeader;
   }
 
-  public boolean isSendMeta() {
-LOG.info("isSendMeta() ? " + sendMeta);
+  public boolean isSendMeta()
+  {
     return sendMeta;
   }
 
-  public void setSendMeta(final boolean sendMeta) {
+  public void setSendMeta(final boolean sendMeta)
+  {
     this.sendMeta = sendMeta;
   }
 
-  public boolean isSendScript() {
+  public boolean isSendScript()
+  {
     return sendScript;
   }
 
-  public void setSendScript(final boolean sendScript) {
-LOG.info("isSendScript() ? " + sendScript);
+  public void setSendScript(final boolean sendScript)
+  {
     this.sendScript = sendScript;
   }
 
-  public String getCompatible() {
-LOG.info("getCompatible() ? " + compatible);
-    if (StringUtils.isEmpty(compatible)) {
+  public String getCompatible()
+  {
+    if (StringUtils.isEmpty(compatible))
+    {
       return DEFAULT_COMPATIBLE;
     }
 
     return compatible;
   }
 
-  public void setCompatible(final String compatible) {
-    if (StringUtils.isEmpty(compatible)) {
+  public void setCompatible(final String compatible)
+  {
+    if (StringUtils.isEmpty(compatible))
+    {
       this.compatible = DEFAULT_COMPATIBLE;
     }
-    else {
+
+    else
+    {
       this.compatible = compatible;
     }
   }
